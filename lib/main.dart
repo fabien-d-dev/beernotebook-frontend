1634f8626f71c1b7102424f297087e9a47b37a6b
fabien-d-dev <it.nove@pm.me>
2026-02-06 14:53:39 +0100

    style(global): adjust app bar

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,25 @@ class BeerNotebookApp extends StatelessWidget {
       title: 'Beernotebook',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'Quicksand-Medium',
+        fontFamily: 'Quicksand',
+
+        // APP BAR
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          elevation: 0,
+          scrolledUnderElevation: 0,
+          surfaceTintColor: Colors.transparent,
+
+          shape: Border(bottom: BorderSide(color: Color.fromRGBO(243, 242, 247, 1), width: 1)),
+
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 22,
+            fontFamily: 'Quicksand',
+          ),
+        ),
+
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 255, 255, 255),
         ),
