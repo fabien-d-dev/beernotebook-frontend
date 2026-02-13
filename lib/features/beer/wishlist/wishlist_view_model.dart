b5d77d22c8780f29f016127cc9a3ad97db12316d
fabien-d-dev <it.nove@pm.me>
2026-02-13 14:10:50 +0100

    feat(wishlist): implement WishlistViewModel with loadWishlist logic

---
+++ lib/features/beer/wishlist/wishlist_view_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../../core/api/api_client.dart';
+import 'wishlist_model.dart';
+
+class WishlistViewModel extends ChangeNotifier {
+  final ApiClient _apiClient;
+  final int wishlistId;
+
+  WishlistViewModel(this._apiClient, this.wishlistId);
+
+  List<WishlistItem> _wishlistBeers = [];
+  List<WishlistItem> get wishlistBeers => _wishlistBeers;
+  bool isLoading = false;
+
+  Future<void> loadWishlist() async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      final List<dynamic> rawData = await _apiClient.getWishlistBeers(
+        wishlistId,
+      );
+      _wishlistBeers = rawData
+          .map((json) => WishlistItem.fromJson(json))
+          .toList();
+    } catch (e) {
+      debugPrint("Error Wishlist: $e");
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
