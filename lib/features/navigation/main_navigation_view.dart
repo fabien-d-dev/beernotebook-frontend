b8dd0367a5dd535384920aafaf0364ef755dbf5a
fabien-d-dev <it.nove@pm.me>
2026-02-18 14:55:22 +0100

    feat(navigation): navigation arrangement
    - BeerView
    - HomeView
    - ProfileView

--- lib/features/navigation/main_navigation_view.dart
+++ lib/features/navigation/main_navigation_view.dart
@@ -10,43 +10,65 @@ class MainNavigationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // We listen to the ViewModel for the current index
     final navVM = context.watch<NavigationViewModel>();
 
-    // List of views corresponding to the tabs
     final List<Widget> pages = [
-      const BeerView(), // Index 0
-      const HomeView(), // Index 1
-      const ProfileView() // Index 2
+      const BeerView(),
+      const HomeView(),
+      const ProfileView(),
     ];
 
     return Scaffold(
       body: IndexedStack(index: navVM.selectedIndex, children: pages),
-      // We use NavigationBar instead of BottomNavigationBar
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: navVM.selectedIndex,
-        onDestinationSelected: (int index) {
-          // We call our ViewModel instead of doing a local setState
-          navVM.setIndex(index);
-        },
-        indicatorColor: const Color.fromARGB(255, 153, 255, 253),
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.sports_bar),
-            icon: Icon(Icons.sports_bar_outlined),
-            label: 'Bières',
+
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          // Text Selected
+          labelTextStyle: WidgetStateProperty.all(
+            const TextStyle(
+              color: Color.fromARGB(255, 66, 66, 66),
+              fontWeight: FontWeight.w500,
+            ),
           ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Accueil',
+          // Icon selected
+          iconTheme: WidgetStateProperty.all(
+            const IconThemeData(color: Color.fromARGB(255, 30, 30, 30)),
           ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.person),
-            icon: Icon(Icons.person_outline),
-            label: 'Profil',
-          ),
-        ],
+        ),
+
+        child: NavigationBar(
+          backgroundColor: Colors.white,
+          selectedIndex: navVM.selectedIndex,
+          onDestinationSelected: (int index) => navVM.setIndex(index),
+          indicatorColor: const Color.fromARGB(255, 176, 238, 255),
+
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.sports_bar),
+              icon: Icon(
+                Icons.sports_bar_outlined,
+                color: Color.fromARGB(255, 56, 56, 56),
+              ),
+              label: 'Bières',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(
+                Icons.home_outlined,
+                color: Color.fromARGB(255, 56, 56, 56),
+              ),
+              label: 'Accueil',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.person),
+              icon: Icon(
+                Icons.person_outline,
+                color: Color.fromARGB(255, 56, 56, 56),
+              ),
+              label: 'Profil',
+            ),
+          ],
+        ),
       ),
     );
   }
