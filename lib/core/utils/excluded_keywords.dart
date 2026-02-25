ecaf1b5b164379c64ca6f5ea736bfd9596521f8c
fabien-d-dev <it.nove@pm.me>
2026-02-25 17:51:33 +0100

    chore(scan): excluded Keywords - en - fr - es

---
+++ lib/core/utils/excluded_keywords.dart
@@ -0,0 +1,88 @@
+// lib/core/utils/excluded_keywords.dart
+
+const Set<String> excludedKeywords = {
+  // Boissons et aliments non liés à la bière
+  "vin",
+  "whisky",
+  "alimentation",
+  "food",
+  "spiritueux",
+  "liqueur",
+  "wine", "vodka", "cognac", "rum", "rhum", "gin", "champagne", "soda", "jus",
+  "alimento",
+  "comida",
+  "pan",
+  "vino",
+  "accesorio",
+  "Bebida sin alcohol",
+  "sin alcohol",
+  "sin-alcohol",
+  "drink", "cola", "thé", "café", "chocolat", "liqueurs", "apéritif",
+  "digestif", "cocktail", "energy drink", "boisson énergisante", "jus de fruit",
+  "cidre",
+  "eau",
+  "eau minérale",
+  "mineral water",
+  "soft drink",
+  "non-alcoolisé",
+  "non-alcoholic", "spirits", "tequila", "mezcal", "martini", "vermouth",
+  "bridelight",
+  "dairie",
+  "laiterie",
+  "aliment",
+  "nutriscore",
+  "agriculture",
+  "farming",
+  "palm oil",
+  "palm",
+  "oil",
+  "huile de palme",
+  "huile",
+  "palme",
+  "0-0",
+  "san",
+  "tourtel",
+  "bordeaux",
+  "desalcoolise",
+  "rosee",
+  // Plats préparés
+  "plats préparés", "prepared dishes", "prêts à manger", "ready-to-eat meals",
+  // Pain et pâtisserie
+  "pain", "bread", "pâtisserie", "pastry", "boulangerie", "bakery",
+  // Sauces et assaisonnements
+  "sauces", "assaisonnements", "seasonings",
+  // Condiments et épices
+  "condiments", "épices", "spices", "herbes", "herbs",
+  // Sucreries et desserts
+  "confiseries", "confectionery", "sucreries",
+  // Grains et céréales
+  "grains",
+  // Noix et graines
+  "noix", "nuts", "graines", "seeds",
+  // Viandes
+  "viandes", "meats", "viande", "meat", "charcuterie",
+  "poulet", "chicken", "bœuf", "beef", "porc", "pork", "agneau", "lamb",
+  "dinde", "turkey", "canard", "duck", "lapin", "rabbit", "jambon", "ham",
+  "saucisse", "sausage", "bacon", "lardon",
+  // Produits laitiers
+  "produits laitiers",
+  "dairy products",
+  "fromagerie",
+  "cheese shop",
+  "crèmerie",
+  "dairy shop",
+  // Poissons et fruits de mer
+  "fruits de mer", "seafood",
+  "poisson", "fish", "saumon", "salmon", "thon", "tuna", "crevette", "shrimp",
+  "crabe", "crab", "huître", "oyster", "moule", "mussel", "calamar", "squid",
+  "homard", "lobster",
+  // Produits laitiers
+  "lait", "milk", "fromage", "cheese", "yaourt", "yogurt", "beurre", "butter",
+  "crème", "cream", "crème glacée", "ice cream",
+  // Légumes
+  "carotte", "carrot", "tomate", "tomato", "concombre", "cucumber",
+  "laitue", "lettuce", "poivron", "pepper", "chou", "cabbage",
+  "brocoli", "broccoli", "épinard", "spinach", "oignon", "onion",
+  "ail", "garlic", "poireau", "leek", "pomme de terre", "potato",
+  "haricot", "bean", "petit pois", "pea", "maïs", "corn",
+};
